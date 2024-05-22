0c6bb552679fbd3fd4b7d25e36e1f135394c5848
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-22 18:59:00 +0000

    added class Task to models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from datetime import datetime, timedelta
+from enum import Enum
 
 db = SQLAlchemy()
 
@@ -16,4 +19,37 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+def default_due_date():
+    return datetime.now() + timedelta(days=2)
+
+class StatusEnum(Enum):
+    PENDING = "pending"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=False, nullable=False)
+    description = db.Column(db.String(500), unique=False, nullable=False)
+    creation_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+    due_date = db.Column(db.DateTime, default=default_due_date, nullable=False)
+    status = db.Column(db.Enum(StatusEnum), nullable=False, default=StatusEnum.PENDING)
+    delivery_location = db.Column(db.String(120), nullable=False)
+    pickup_location = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "creation_date": self.creation_date.isoformat(),
+            "due_date": self.due_date.isoformat(),
+            "status": self.status.value,
+            "delivery_location": self.delivery_location,
+            "pickup_location": self.pickup_location
         }
