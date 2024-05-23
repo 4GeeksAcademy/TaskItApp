37cdc09d46c84fdfa6641b2dc4ad44561ba724f7
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-23 16:30:00 +0000

    moved modal into task_form

--- src/front/js/component/task_feed/task_form.jsx
+++ src/front/js/component/task_feed/task_form.jsx
@@ -1,29 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { Modal, ModalTitle } from 'react-bootstrap';
+import { Context } from '../../store/appContext.js';
+import Alert from '../alert.jsx';
 
-const TaskForm = ({ handleSubmit }) => {
+const TaskForm = (props) => {
+    const { store, actions } = useContext(Context);
+
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [pickupAddress, setPickupAddress] = useState('');
     const [deliveryAddress, setDeliveryAddress] = useState('');
     const [dueDate, setDueDate] = useState('');
 
+    useEffect(() => {
+        if(props.currentTask) {
+            setTitle(props.currentTask.title);
+            setDescription(props.currentTask.description);
+            setPickupAddress(props.currentTask.pickup_location);
+            setDeliveryAddress(props.currentTask.delivery_location);
+            setDueDate(props.currentTask.due_date.split("T")[0]);
+        }
+    }, [])
+
+    const handleSubmit = (title, description, pickupAddress, deliveryAddress, dueDate) => {
+        event.preventDefault();
+        console.log(dueDate)
+        if(props.currentTask) actions.editTask(props.currentTask.id, title, description, pickupAddress, deliveryAddress, dueDate);
+        else actions.addTask(title, description, pickupAddress, deliveryAddress, dueDate);
+    };
+
     return (
-        <form id="task-form" onSubmit={() => handleSubmit(title, description, pickupAddress, deliveryAddress, dueDate)}>
-            <label htmlFor='title'>Title</label>
-            <input type="text" className="form-control" placeholder="Title" aria-label="Title" id='title' aria-describedby="basic-addon1" value={title} onChange={(e) => setTitle(e.target.value)} />
+        <Modal show={props.show} aria-labelledby="contained-modal-title-vcenter" centered>
+            <Modal.Header>
+                <ModalTitle>Task Information</ModalTitle>
+                <button className="btn close" data-dismiss="modal" aria-label="Close" onClick={props.handleClose}>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </Modal.Header>
+            <Modal.Body>
+                {(store.message || store.error) && <Alert message={store.message} error={store.error} ></Alert>}
+                <form id="task-form" onSubmit={() => handleSubmit(title, description, pickupAddress, deliveryAddress, dueDate)}>
+                    <label htmlFor='title'>Title</label>
+                    <input type="text" className="form-control" placeholder="Title" aria-label="Title" id='title' aria-describedby="basic-addon1" value={title} onChange={(e) => setTitle(e.target.value)} />
 
-            <label htmlFor='description'>Description</label>
-            <textarea className="form-control" aria-label="description" maxLength="500" rows="5" value={description} onChange={(e) => setDescription(e.target.value)} ></textarea>
-            
-            <label htmlFor='pickup-address'>Pickup Address</label>
-            <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="pickup address" id='pìckup-address' aria-describedby="basic-addon1" value={pickupAddress} onChange={(e) => setPickupAddress(e.target.value)} />
+                    <label htmlFor='description'>Description</label>
+                    <textarea className="form-control" aria-label="description" maxLength="500" rows="5" value={description} onChange={(e) => setDescription(e.target.value)} ></textarea>
+                    
+                    <label htmlFor='pickup-address'>Pickup Address</label>
+                    <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="pickup address" id='pìckup-address' aria-describedby="basic-addon1" value={pickupAddress} onChange={(e) => setPickupAddress(e.target.value)} />
 
-            <label htmlFor='delivery-address'>Delivery Address</label>
-            <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="delivery address" id='delivery-address' aria-describedby="basic-addon1" value={deliveryAddress} onChange={(e) => setDeliveryAddress(e.target.value)} />
+                    <label htmlFor='delivery-address'>Delivery Address</label>
+                    <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="delivery address" id='delivery-address' aria-describedby="basic-addon1" value={deliveryAddress} onChange={(e) => setDeliveryAddress(e.target.value)} />
 
-            <label htmlFor='due-date'>Due Date</label>
-            <input type="date" className="form-control" aria-label="due date" id='due-date' aria-describedby="basic-addon1" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-        </form>
+                    <label htmlFor='due-date'>Due Date</label>
+                    <input type="date" className="form-control" aria-label="due date" id='due-date' aria-describedby="basic-addon1" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+                </form>
+            </Modal.Body>
+            <Modal.Footer>
+                <button type="button" className="btn btn-secondary" onClick={props.handleClose}>Close</button>
+                <button type="submit" form="task-form" className="btn btn-primary">Post</button>
+            </Modal.Footer>
+        </Modal>
     );
 }
 
