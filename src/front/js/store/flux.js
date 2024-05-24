f6653e716751304c57a27b4fabc76f0c39ad3119
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-05-24 18:32:46 +0000

    Add category management actions to global store

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,84 +2,71 @@ const getState = ({ getStore, getActions, setStore }) => {
 	const fetchHelper = async (url, config = {}, successCallback) => {
 		try {
 			const response = await fetch(url, config);
+			const data = await response.json();
 			if (response.ok) {
-				const data = await response.json();
 				if (successCallback) successCallback(data);
-				setStore({ message: response.message || "", error: "" });
-			} else setStore({ message: "", error: response.error || "An error occurred" });
-			console.log(getStore())
+				setStore({ message: data.message || "", error: "" });
+			} else setStore({ message: "", error: data.error || "An error occurred" });
 		} catch (error) {
 			console.error(error);
 		}
+		
 	};
 
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			error: "",
+			categories: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			
-			getTasks: () => {
+			getCategories: () => {
 				fetchHelper(
-					process.env.BACKEND_URL + "/api/tasks", // url como siempre
+					process.env.BACKEND_URL + "/api/categories", // url como siempre
 					{}, 									// la configuración del request, en este caso vacía porque es un GET
-					(data) => setStore({ tasks: data })		// función a realizar despues de que una respuesta sea buena
+					(data) => setStore({ categories: data.categories })		// función a realizar despues de que una respuesta sea buena
 				)
 			},
 
-			deleteTask: (id) => {
+			deleteCategory: (id) => {
 				const config = { 
 					method: "DELETE",
 					headers: { 'Accept': 'application/json' }
 				}
 
 				fetchHelper(
-					process.env.BACKEND_URL + `/api/tasks/${id}`,
+					process.env.BACKEND_URL + `/api/categories/${id}`,
 					config,
-					() => getActions().getTasks(),
+					() => getActions().getCategories(),
+				)
+			},
+			editCategory: (id, name) => {
+				const config = { 
+					method: "PUT",
+					body: JSON.stringify({"name":name}),
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					}
+				}
+				fetchHelper(
+					process.env.BACKEND_URL + `/api/categories/${id}`,
+					config,
+					() => getActions().getCategories(),
+				)
+			},addCategory: (name) => {
+				const config = { 
+					method: "POST",
+					body: JSON.stringify({"name":name}),
+					headers: {
+						'Accept': 'application/json',
+						'Content-Type': 'application/json'
+					}
+				}
+				fetchHelper(
+					process.env.BACKEND_URL + `/api/categories`,
+					config,
+					() => getActions().getCategories(),
 				)
 			},
 		}
