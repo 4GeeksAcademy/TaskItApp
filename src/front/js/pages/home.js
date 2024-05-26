3de7b655e9eb5d9b40753c94a7872d86c5ef8a58
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-26 07:35:27 +0000

    added buttons to every path

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,12 +2,19 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
+			<button className="btn btn-primary" onClick={() => actions.setAuth(!store.auth)}>Auth</button>
+			<Link className="px-5" to="/users">Users</Link>
+			<Link className="px-5" to="/categories">categories</Link>
+			<Link className="px-5" to="/addresses">addresses</Link>
+			<Link className="px-5" to="/requesters">requesters</Link>
+			<Link className="px-5" to="/seekers">seekers</Link>
 			<h1>Hello Rigo!!</h1>
 			<p>
 				<img src={rigoImageUrl} />
