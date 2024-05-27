95d1949fec18563a80a12bf131166ce36b7b6189
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-27 03:08:21 +0000

    select to choose user for testing purposes

---
+++ src/front/js/component/user_list_select.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+
+const UserSelect = () => {
+    const { store, actions } = useContext(Context);
+    const [selectedValue, setSelectedValue] = useState('');
+
+    useEffect(() => {
+        actions.getUsers();
+    }, []);
+
+
+    return (
+        <div className="d-flex align-items-center justify-content-center">
+            <div className="fw-bold pe-2">Choose user:</div>
+            <select 
+                className="form-select select-options" 
+                aria-label="User list" 
+                id="user-select"
+                value={selectedValue}
+                onChange={(e) => { actions.setUser(e.target.value); setSelectedValue(e.target.value); }}
+            >
+                {store.users.map((user) => (
+                    <option key={user.id} value={user.username}>{user.username}</option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default UserSelect;
