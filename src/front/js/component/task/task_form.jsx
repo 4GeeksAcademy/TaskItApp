dfc641ead136b8da191da094810c529387647346
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-05-28 15:35:37 +0000

    added budget and address component to task form

--- src/front/js/component/task/task_form.jsx
+++ src/front/js/component/task/task_form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Modal, ModalTitle } from 'react-bootstrap';
 import { Context } from '../../store/appContext.js';
 import Alert from '../alert.jsx';
+import AddressInput from '../geocoding/address_input.jsx'
 
 const TaskForm = (props) => {
     const { store, actions } = useContext(Context);
@@ -12,6 +13,7 @@ const TaskForm = (props) => {
     const [deliveryAddress, setDeliveryAddress] = useState('');
     const [dueDate, setDueDate] = useState('');
     const [category, setCategory] = useState('');
+    const [budget, setBudget] = useState(0);
 
     useEffect(() => {
         if (Object.keys(store.categories).length == 0) actions.getCategories();
@@ -27,8 +29,16 @@ const TaskForm = (props) => {
 
     const handleSubmit = () => {
         event.preventDefault();
-        if(props.currentTask) actions.editTask(props.currentTask.id, title, description, pickupAddress, deliveryAddress, dueDate);
-        else actions.addTask(title, description, pickupAddress, deliveryAddress, dueDate, category);
+        if(props.currentTask) actions.editTask(props.currentTask.id, title, description, pickupAddress, deliveryAddress, dueDate, category, null, budget);
+        else actions.addTask(title, description, pickupAddress, deliveryAddress, dueDate, category, budget);
+    };
+
+    const handlePickupAddressChange = (newAddress) => {
+        setPickupAddress(newAddress);
+    };
+    
+    const handleDeliveryAddressChange = (newAddress) => {
+        setDeliveryAddress(newAddress);
     };
 
     return (
@@ -42,7 +52,7 @@ const TaskForm = (props) => {
             <Modal.Body>
                 {(store.message || store.error) && <Alert message={store.message} error={store.error} ></Alert>}
 
-                <form id="task-form" onSubmit={() => handleSubmit()}>
+                <form id="task-form">
                     <label htmlFor='title'>Title</label>
                     <input type="text" className="form-control" placeholder="Title" aria-label="Title" id='title' aria-describedby="basic-addon1" value={title} onChange={(e) => setTitle(e.target.value)} />
 
@@ -52,7 +62,7 @@ const TaskForm = (props) => {
                         aria-label="category list" 
                         id="category-select"
                         value={category}
-                        onChange={(e) => {setCategory(e.target.value); console.log(store.categories, e.target.value)}}
+                        onChange={(e) => setCategory(e.target.value)}
                     >
                         <option value="" disabled>Choose category</option>
                         {store.categories.map((category) => (
@@ -63,19 +73,29 @@ const TaskForm = (props) => {
                     <label htmlFor='description'>Description</label>
                     <textarea className="form-control" aria-label="description" maxLength="500" rows="5" value={description} onChange={(e) => setDescription(e.target.value)} ></textarea>
                     
-                    <label htmlFor='pickup-address'>Pickup Address</label>
-                    <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="pickup address" id='pìckup-address' aria-describedby="basic-addon1" value={pickupAddress} onChange={(e) => setPickupAddress(e.target.value)} />
+                    <AddressInput label="Pickup Address" handleChange={handlePickupAddressChange} value={pickupAddress} />
+                    <AddressInput label="Delivery Address" handleChange={handleDeliveryAddressChange} value={deliveryAddress} />
 
-                    <label htmlFor='delivery-address'>Delivery Address</label>
-                    <input type="text" className="form-control" placeholder="4 Privet Drive" aria-label="delivery address" id='delivery-address' aria-describedby="basic-addon1" value={deliveryAddress} onChange={(e) => setDeliveryAddress(e.target.value)} />
-
-                    <label htmlFor='due-date'>Due Date</label>
-                    <input type="date" className="form-control" aria-label="due date" id='due-date' aria-describedby="basic-addon1" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+                    <div className='d-flex justify-content-between'>
+                        <div>
+                            <label htmlFor='due-date'>Due Date</label>
+                            <input type="date" className="form-control" aria-label="due date" id='due-date' aria-describedby="basic-addon1" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor='budget'>Budget</label>
+                            <div className='input-group'>
+                                <input type="text" className="form-control" id='budget' aria-label="budget" aria-describedby="basic-addon1" value={budget} onChange={(e) => setBudget(e.target.value)} />
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="basic-addon2">€</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </Modal.Body>
             <Modal.Footer>
                 <button type="button" className="btn btn-secondary" onClick={props.handleClose}>Close</button>
-                <button type="submit" form="task-form" className="btn btn-primary">Post</button>
+                <button type="submit" form="task-form" className="btn btn-primary" onClick={handleSubmit}>Post</button>
             </Modal.Footer>
         </Modal>
     );
