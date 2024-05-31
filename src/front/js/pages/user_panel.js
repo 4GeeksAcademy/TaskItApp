6b842f8887d3d64ecaef545e17ac5b2571132e66
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-05-31 18:37:36 +0000

    Se actualiza con boton logout

--- src/front/js/pages/user_panel.js
+++ src/front/js/pages/user_panel.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 const UserPanel = () => {
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        actions.logout();
+        navigate("/login-user");
+    };
+
     return (
         <div className="container mt-5">
             <h1>Este es el panel de usuario</h1>
+            <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
         </div>
     );
 }
