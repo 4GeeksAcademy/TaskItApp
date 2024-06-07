e116cbd136d221da83336d2027bacc4a6a1752ec
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-07 15:24:35 +0000

    fixed undefined errors

--- src/front/js/component/chat/message.jsx
+++ src/front/js/component/chat/message.jsx
@@ -4,12 +4,15 @@ import { Context } from '../../store/appContext';
 const Message = ({ message, markMessageAsSeen }) => {
     const { store } = useContext(Context);
 
+    const isCurrentUserMessage = message && message.sender_user && message.sender_user.username === store.user.username;
+    const isUserMessage = message && message.username && message.username === store.user.username;
+
     useEffect(() => {
         markMessageAsSeen(message.id)
     }, []);
 
     return (
-        <div className={`row border rounded col-11 my-2 mx-1 p-1 text-white ${(message.sender_user.username == store.user.username || message.username == store.user.username) ? "float-end bg-primary" : "bg-secondary"}`}>
+        <div className={`row border rounded col-11 my-2 mx-1 p-1 text-white ${(isCurrentUserMessage || isUserMessage) ? "float-end bg-primary" : "bg-secondary"}`}>
             <span>{message.message}</span>
         </div>
     );
