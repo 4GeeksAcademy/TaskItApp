2ffc7d4d9eb785eb7f11f978ef5c2ebde70f1453
Aresdgi <115067719+Aresdgi@users.noreply.github.com>
2024-06-07 06:39:39 +0000

    Add modelview AdminUser

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Task, Category, Address, Requester, TaskSeeker, Rating, Postulant
+from .models import db, User, Task, Category, Address, Requester, TaskSeeker, Rating, Postulant, AdminUser
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -19,5 +19,8 @@ def setup_admin(app):
     admin.add_view(ModelView(TaskSeeker, db.session))
     admin.add_view(ModelView(Rating, db.session))
     admin.add_view(ModelView(Postulant, db.session))
+    admin.add_view(ModelView(AdminUser, db.session))
+
+
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
