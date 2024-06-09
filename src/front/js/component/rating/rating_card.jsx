e4970baabb18e1cba81617fabda684db67d68eac
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-09 19:00:35 +0000

    now ratings appear on task detail

--- src/front/js/component/rating/rating_card.jsx
+++ src/front/js/component/rating/rating_card.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import StarRating from "./StarRating.jsx"
+import StarRating from "./StarRating.jsx";
+import { useLocation } from "react-router-dom";
 
 const RatingCard = ({ rating }) => {
+    const path = useLocation().pathname;
+    const isTaskDetailPage = /^\/tasks\/\d+$/.test(path);
+
     return (
-        <div className="col-lg-4 col-md-8 col-sm-11 p-2 d-flex flex-column">
-            <div className={`card p-4 h-100 flex-grow-1`}>
-                <StarRating value={rating.stars} />
-                <p>{rating.review}</p>
+        <div className="container-fluid">
+            <div className={`${isTaskDetailPage ? "col-12 w-100 flex-grow-1 " : "col-lg-4 col-md-8 col-sm-11 flex-column"} p-0 d-flex`}>
+                <div className={`card p-4 h-100 flex-grow-1 ${isTaskDetailPage ? "w-100" : ""} `}>
+                    <StarRating value={rating.stars || 0} />
+                    <p>{rating.review}</p>
+                </div>
             </div>
         </div>
     );
 }
 
-export default RatingCard
+export default RatingCard;
