6ee68868e4e0135070533a9966d66d362a751c90
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-09 05:13:37 +0000

    added picture to applied to task

--- src/front/js/component/task/applied_to_task_card.jsx
+++ src/front/js/component/task/applied_to_task_card.jsx
@@ -14,7 +14,14 @@ const AppliedToTask = ({ taskInfo }) => {
         <div className="col-lg-4 col-md-8 col-sm-11 p-2 d-flex flex-column">
             <div className={`card p-4 ${applicantInfo?.status == "accepted" ? "border border-success" : applicantInfo?.status == "rejected" ? "border border-danger" : ""} h-100 flex-grow-1`}>
                 <div className="d-flex align-items-center mb-2">
-                    <div className="rounded-circle bg-dark me-2" style={{ height: "60px", width: "60px" }}></div>
+                    <div className="rounded-circle bg-dark me-2" style={{ height: "60px", width: "60px" }}>
+                        { taskInfo.requester_user?.profile_picture && <img
+                            className="img-fluid"
+                            src={taskInfo.requester_user?.profile_picture}
+                            alt="User Profile"
+                            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                        />}
+                    </div>
                     <div className="d-flex flex-column justify-content-around">
                         <span className="fs-5">
                             <Link to={`/users/${taskInfo.requester_user.username}`}><b>{taskInfo.requester_user.username} </b></Link> 
