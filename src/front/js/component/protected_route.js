e70f5624c163bdd998de0fdd62010cc2f123c8a0
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-06-11 20:54:27 +0000

    Se mejora el codigo de proteccion sin dar fallos

--- src/front/js/component/protected_route.js
+++ src/front/js/component/protected_route.js
@@ -1,30 +1,11 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-const ProtectedRoute = ({ element, roles }) => {
-    const { store, actions } = useContext(Context);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isAuthorized, setIsAuthorized] = useState(false);
+const ProtectedRoute = ({ element }) => {
+    const { store } = useContext(Context);
 
-    useEffect(() => {
-        const checkAuthorization = async () => {
-            const tokenValid = await actions.validateToken();
-            if (tokenValid) {
-                if (!roles || roles.includes(store.user?.role)) {
-                    setIsAuthorized(true);
-                }
-            }
-            setIsLoading(false);
-        };
-        checkAuthorization();
-    }, [actions, store.user?.role, roles]);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (!isAuthorized) {
+    if (!store.auth) {
         return <Navigate to="/login" />;
     }
 
