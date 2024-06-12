d558db73e39f664506caa1045836511b6c91fd0d
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-12 06:00:08 +0000

    chat notification badge hidden on /chat and /chats/id

--- src/front/js/component/notification/phone_chat_notification_badge.js
+++ src/front/js/component/notification/phone_chat_notification_badge.js
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react';
 import { Context } from '../../store/appContext';
+import { useLocation } from 'react-router-dom';
 
 const ChatNotification = () => {
     const { store } = useContext(Context);
+    const path = useLocation().pathname;
+    const isChat = /^\/chats\/\d+$/.test(path);
+
     return (
         <div>
-            { Object.values(store.unseenMessages).some((value) => value) && 
+            { (Object.values(store.unseenMessages).some((value) => value) && path != '/chat' && !isChat) && 
                 <span className="position-absolute top-0 end-0 badge rounded-pill bg-danger text-danger" style={{ fontSize: "0.6rem" }}>
                      .
                 </span>
