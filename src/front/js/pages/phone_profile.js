95cdf9cdbfc6309adb78ef9f77b0645b740395e0
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-12 08:06:52 +0000

    added phone view of task and user

---
+++ src/front/js/pages/phone_profile.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import StarRating from "../component/rating/StarRating.jsx";
+import RatingCard from "../component/rating/rating_card.jsx";
+import useScreenWidth from "../hooks/useScreenWidth.jsx";
+
+export const PhoneUser = () => {
+	const { actions } = useContext(Context);
+	const params = useParams();
+    const smallDevice = useScreenWidth();
+
+	const [user, setUser] = useState({});
+	const [reviews, setReviews] = useState([])
+	const [seekerInfo, setSeekerInfo] = useState({});
+	const [requesterInfo, setRequesterInfo] = useState({});
+
+	useEffect(() => {
+		const loadInfo = async () => {
+			const currentUser = await actions.getUserByUsername(params.theusername);
+			setUser(currentUser);
+			
+			if(currentUser.role == "both"|| currentUser.role == "task_seeker") setSeekerInfo(await actions.getSeeker(currentUser.id));
+			if(currentUser.role == "both"|| currentUser.role == "requester") setRequesterInfo(await actions.getRequester(currentUser.id));
+
+			fetchReviews(currentUser)
+		}
+
+		loadInfo();
+	}, [])
+
+	const fetchReviews = async (currentUser) => {
+        try {
+            const response = await fetch(process.env.BACKEND_URL + `/api/users/${currentUser.id}/reviews`); 
+            const data = await response.json();
+            setReviews(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+	return (
+		<div className="containerpt-0">
+			<div className="row d-flex justify-content-center mx-2">
+				<div className="col-12" >
+					<div className="rounded overflow-hidden p-0 float-start">
+						{ user.profile_picture && <img 
+							className="img-fluid" 
+							src={user.profile_picture} 
+							alt="User Profile"
+							style={{ height: "auto", objectFit: "cover" }}
+						/>}
+					</div>
+				</div>
+				<div className="col-12 d-flex flex-column justify-content-between">
+					<div>
+						<h1><b>{user.full_name}</b> ({user.username})</h1>
+						{ requesterInfo && Object.keys(requesterInfo).length > 0 && 
+							(<div className="d-flex justify-content-between">
+								<span className="text-muted fs-2">Requester</span>
+								<div className="d-flex align-items-center">
+									<StarRating value={requesterInfo.overall_rating}></StarRating>
+									<span className="text-muted ms-1">({requesterInfo.total_reviews})</span>
+								</div>
+							</div>)
+						}
+						{ seekerInfo && Object.keys(seekerInfo).length > 0 && 
+							(<div className="d-flex justify-content-between">
+								<span className="text-muted fs-2">Task Seeker</span>
+								<div className="d-flex align-items-center">
+									<StarRating value={seekerInfo.overall_rating}></StarRating>
+									<span className="text-muted ms-1">({seekerInfo.total_reviews})</span>
+								</div>
+							</div>)
+						}
+						<p className="fs-3 text-muted">{user.description}</p>
+					</div>
+					<div className="d-flex flex-column justify-content-around gap-2">
+                        {	requesterInfo && Object.keys(requesterInfo).length > 0 &&
+							<div className="card d-flex justify-content-around flex-row">
+								<div className="text-center d-flex flex-column col-4">
+									<span className="fs-5"><b>Average Budget</b></span>
+									<span className="fs-2 text-muted">{requesterInfo.average_budget.toFixed(2)}</span>
+								</div>
+								<div className="text-center d-flex flex-column col-4">
+									<span className="fs-5"><b>Total Requested</b></span>
+									<span className="fs-2 text-muted">{requesterInfo.total_requested_tasks}</span>
+								</div>
+								<div className="text-center d-flex flex-column col-4 align-self-end">
+									<span className="fs-5"><b>Open</b></span>
+									<span className="fs-2 text-muted">{requesterInfo.total_open_tasks}</span>
+								</div>
+							</div>
+						}
+						{	seekerInfo && Object.keys(seekerInfo).length > 0 &&
+							<div className="card d-flex justify-content-around flex-row">
+								<div className="text-center d-flex flex-column">
+									<span className="fs-5"><b>Total Completed</b></span>
+									<span className="fs-2 text-muted">{seekerInfo.total_completed_tasks}</span>
+								</div>
+								<div className="text-center d-flex flex-column">
+									<span className="fs-5"><b>Ongoing</b></span>
+									<span className="fs-2 text-muted">{seekerInfo.total_ongoing_tasks}</span>
+								</div>
+							</div>
+						}
+					</div>
+				</div>
+				{ reviews.length > 0 && <hr className="my-5"></hr> }
+                    <div className={`d-flex justify-content-between ${smallDevice ? "flex-column gap-3" : ""}`}>
+                        { reviews.map((review) =>{
+                            return <RatingCard key={review.id + 'urev'} rating={review} />
+                        })}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+PhoneUser.propTypes = {
+	match: PropTypes.object
+};
---
+++ src/front/js/pages/phone_task.js
@@ -0,0 +1,130 @@
+import React, { useState, useEffect, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link, useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+import StarRating from "../component/rating/StarRating.jsx";
+import Map from "../component/geocoding/map.jsx";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import TaskForm from "../component/task/task_form.jsx";
+import ApplicantForm from "../component/applicant/applicant_form.jsx"
+import RatingCard from "../component/rating/rating_card.jsx";
+
+export const PhoneTask = () => {
+	const { store, actions } = useContext(Context);
+	const params = useParams();
+	
+	const [task, setTask] = useState({});
+	const [requester, setRequester] = useState({});
+	const [showTaskForm, setShowTaskForm] = useState(false);
+	const [showApplicationForm, setShowApplicationForm] = useState(false);
+	const [applied, setApplied] = useState(false);
+	const [reviews, setReviews] = useState([])
+
+    const handleCloseTaskForm = () => setShowTaskForm(false);
+    const handleShowTaskForm = () => setShowTaskForm(true);
+	const handleCloseApplicationForm = () => setShowApplicationForm(false);
+    const handleShowApplicationForm = () => setShowApplicationForm(true);
+
+	const loadInfo = async () => {
+		const currentTask = await actions.getTask(params.theid);
+		setTask(currentTask);
+
+		const requesterData = await actions.getRequester(currentTask.requester_id);
+		setRequester(requesterData);
+		
+		fetchReviews(currentTask);
+
+		alreadyApplied(currentTask);	
+	}
+
+	useEffect(() => {
+		loadInfo();	
+	}, [])
+
+	const alreadyApplied = (currentTask) => {
+		for(let applicant of currentTask.applicants) {
+			if(applicant.seeker.id == store.user.seeker?.id) setApplied(true);
+		}
+	}
+
+	const dueIn = (isoTime) => {
+		const now = new Date();
+		const time = new Date(isoTime);
+		const diff = time - now; 
+	
+		const seconds = Math.floor(diff / 1000);
+	
+		const intervals = {
+			day: 86400,
+			hour: 3600,
+			minute: 60
+		};
+	
+		for (const [unit, secondsInterval] of Object.entries(intervals)) {
+			const intervalCount = Math.floor(seconds / secondsInterval);
+			if (intervalCount >= 1) {
+				if (unit === 'day') return `in ${intervalCount} day${intervalCount === 1 ? '' : 's'} and ${Math.floor((seconds % secondsInterval) / 3600)} hours`;
+				else if (unit === 'hour') return `in ${intervalCount} hour${intervalCount === 1 ? '' : 's'} and ${Math.floor((seconds % secondsInterval) / 60)} minutes`;
+				else return `in ${intervalCount} minute${intervalCount === 1 ? '' : 's'}`;
+			}
+		}
+	
+		return 'Just now';
+	}
+
+	return (
+		<div className="container p-5 pt-0 mx-auto">
+                <div>
+					<div className="d-flex justify-content-between flex-row">
+						<h2>{task.title}</h2>
+						<small className="text-muted"><b>ID: </b>{task.id}</small>
+					</div>
+					<p className="text-muted">{actions.timeAgo(task.creation_date)}</p>
+					<p>{task.description}</p>
+				</div>
+				<div className="card p-2">
+					<div className="d-flex flex-column">
+						<span><Icon className="fs-3" icon="mingcute:location-2-line" /><b>Pick Up: </b>{task.pickup_address?.address}</span>
+						<span><Icon className="fs-3" icon="mingcute:location-2-fill" /><b>Drop Off: </b>{task.delivery_address?.address}</span>
+						<span><Icon className="fs-4 me-2" icon="fa6-solid:money-bill-1-wave" /><b>Budget: </b>{task.budget}€</span>
+						<span><Icon className="fs-3 me-2" icon="solar:calendar-linear" /><b>Due: </b>{dueIn(task.due_date)}</span>
+					</div>
+				</div>
+                <div className="rounded overflow-hidden card p-0  mt-3" style={{ height: "200px"}}>
+					{ (task.delivery_address && task.pickup_address) && <Map markers={[[task.delivery_address?.latitude, task.delivery_address?.longitude], [task.pickup_address?.latitude, task.pickup_address?.longitude]]} height={200}></Map>}
+				</div>
+                <div className="d-flex flex-row justify-content-center align-items-center mt-3 card py-3">
+                        <div className="rounded-circle bg-dark me-2 overflow-hidden" style={{ height: "60px", width: "60px", aspectRatio: "1/1" }}>
+                            { task.requester_user?.profile_picture && <img
+                                className="img-fluid"
+                                src={task.requester_user?.profile_picture}
+                                alt="User Profile"
+                                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                            />}
+                        </div>
+                        <div className="d-flex flex-column justify-content-around">
+                            <Link to={`/users/${task.requester_user?.username}`}>
+                                <h5 className="mb-0 pt-2" >{task.requester_user?.username || "deleted"}</h5>
+                            </Link>
+                            <div className="d-flex align-items-center">
+                                {requester.overall_rating && <StarRating value={requester.overall_rating || 0}></StarRating>}
+                                {parseInt(requester.overall_rating) > 0 && <span className="text-muted ms-1">({requester.total_reviews})</span>}
+                            </div>
+                        </div>
+                    </div>
+				<div className="row d-flex justify-content-center mt-4 px-2">
+					{store.user.id != task.requester_user?.id 
+						? ((store.user.role == "both" || store.user.role == "task_seeker") && !applied) && <button className="btn btn-green px-4" onClick={handleShowApplicationForm}>Apply</button>
+						: task.status == "pending" && <button className="btn btn-green px-4" onClick={handleShowTaskForm}><Icon icon="mage:edit-fill" /> Edit</button>
+					}
+				</div>
+			
+			<TaskForm show={showTaskForm} handleClose={handleCloseTaskForm} currentTask={task} loadInfo={loadInfo} ></TaskForm>
+			<ApplicantForm show={showApplicationForm} handleClose={handleCloseApplicationForm} taskID={task.id} setApplied={setApplied} taskRequester={task.requester_user?.username}></ApplicantForm>
+		</div>
+	);
+};
+
+PhoneTask.propTypes = {
+	match: PropTypes.object
+};
