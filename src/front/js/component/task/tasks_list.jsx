ba15a8c7bf8634a53fc5d6a92189e298a0c86ca7
Blondy Martínez <47917655+BlondyMartinez@users.noreply.github.com>
2024-06-14 07:22:35 +0000

    ability to sort by property and order

--- src/front/js/component/task/tasks_list.jsx
+++ src/front/js/component/task/tasks_list.jsx
@@ -1,16 +1,41 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../store/appContext";
 import Task from "./task_card.jsx";
+import SortBy from "../sort_by.jsx";
 
 const TaskList = () => {
     const { store, actions } = useContext(Context);
+    const [tasks, setTasks] = useState([]);
 
-    useEffect(() => actions.getTasks(), []);
+    useEffect(() => actions.getTasks(), []);  
+    useEffect(() => setTasks(store.tasks), [store.tasks]);    
+
+    const handleSort = (sortKey, sortOrder) => {
+        const sortedTasks = [...tasks].sort((a, b) => {
+            if (!a.hasOwnProperty(sortKey) || !b.hasOwnProperty(sortKey)) return 0; 
+            if (sortKey === "budget") {
+                const aValue = parseFloat(a[sortKey]) || 0;
+                const bValue = parseFloat(b[sortKey]) || 0;
+                return sortOrder === 'asc' ? aValue - bValue : bValue - aValue;
+            }
+    
+            if (typeof a[sortKey] === 'number' && typeof b[sortKey] === 'number') {
+                return sortOrder === 'asc' ? a[sortKey] - b[sortKey] : b[sortKey] - a[sortKey];
+            } else {
+                const aValue = a[sortKey].toString();
+                const bValue = b[sortKey].toString();
+                return sortOrder === 'asc' ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+            }
+        });
+    
+        setTasks(sortedTasks);
+    };
 
     return (
         <div className="container-fluid px-5 bg-light">
+            <SortBy onSort={handleSort} />
             <div className="row">
-                {store.tasks.slice().reverse().map((task) => {
+                {tasks.map((task) => {
                     return (
                         <React.Fragment key={task.id + "tl"}>
                             <Task taskInfo={task}></Task>
